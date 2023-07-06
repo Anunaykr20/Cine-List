1026cd9a5b1820e3ec24e456fe6ebcb6243966d7
Anunay Kumar <anunaylife@gmail.com>
2023-07-06 11:33:12 +0530

    subtitle change

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -41,8 +41,7 @@ const HeroBanner = () => {
                 <div className="heroBannerContent">
                     <span className="title">Welcome.</span>
                     <span className="subTitle">
-                        Millions of movies, TV shows and people to discover.
-                        Explore now.
+                        A project by Anunay Kumar.
                     </span>
                     <div className="searchInput">
                         <input
